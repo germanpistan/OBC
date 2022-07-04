55eeacf951fe5a7185ce449e18cfe6d009869fb5
ematevez <70983593+ematevez@users.noreply.github.com>
2022-07-04 14:07:31 -0300

    Add files via upload

---
+++ Python-Ejercicios/Ejercicio_3.py
@@ -0,0 +1,17 @@
+#En este ejercicio tendrás que crear una variable a
+# la cual asignarás una cadena de texto y la imprimirás
+# por consola, a su vez, tendrás que imprimir también el
+# tipo de la variable por consola.
+# Este ejercicio lo tienes que realizar desde la consola
+# de python y subir capturas de pantalla en una carpeta comprimida zip.
+
+#Modifica la variable del anterior ejercicio en la consola de python y
+# después muestrala por consola para ver la modificación de la variable.
+
+variables = "Soy una cadena de texto"
+
+print( "variable: " + variables)
+print("Tipo de variable:")
+print(type(variables))
+
+
