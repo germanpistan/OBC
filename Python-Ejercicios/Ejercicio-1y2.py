845698bba667fa2f73fabcf4c23cf1f319fd6c93
ematevez <70983593+ematevez@users.noreply.github.com>
2022-07-03 21:19:09 -0300

    Add files via upload

---
+++ Python-Ejercicios/Ejercicio-1y2.py
@@ -0,0 +1,18 @@
+#Ejercicio1
+#Desde la consola de python, debes de imprimir las siguientes cadenas de texto:
+#Hola, soy [tu_nombre]
+#Estoy empezando el curso de python
+#Espero aprender mucho
+#Tienes que subir capturas de pantalla en una carpeta comprimida zip.
+
+#Ejercicio2
+#Desde la consola de python almacena la cadena “Hola mundo!” en una variable y muéstrala.
+
+print("Hola, soy Emanuel")
+print("Estoy empezando el curso de python")
+print("Espero aprender mucho")
+
+#-------------------------------------------------------------
+print("---------------------------")
+var = "Hola mundo"
+print(var)
