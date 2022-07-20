31e6a647f3cb350a9f2da552b34d101aaf550018
ematevez <70983593+ematevez@users.noreply.github.com>
2022-07-19 22:55:01 -0300

    JS EJ01

---
+++ Curso-Javascript/mi-primer-proyecto-de-node/index.js
@@ -0,0 +1,9 @@
+//Mensaje de bienvenida
+console.log("Esta es la entrada al proyecto")
+
+/*Desarrollo del proyecto
+//Chequeo
+//Ultimas lineas*/
+
+//Este es mensaje despedida
+console.log("Adios")
