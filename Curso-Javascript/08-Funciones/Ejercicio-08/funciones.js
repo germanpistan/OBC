ea9bf6c31c2f5b749a36f5c7e17b949e7007c2e4
ematevez <70983593+ematevez@users.noreply.github.comb.comgit config --global user.email 70983593+ematevez@users.noreply.github.comb.com>
2022-07-21 16:32:44 -0300

    EJ08 JS

---
+++ Curso-Javascript/08-Funciones/Ejercicio-08/funciones.js
@@ -0,0 +1,23 @@
+/*Crea un archivo JS que contenga las siguientes líneas
+- Una función sin parámetros que devuelva siempre "true"
+- Una función asíncrona que utilice un setTimeout y pase por 
+consola un "Hola soy una promesa" 5 segundos después de haberse ejecutado
+- Una función generadora de índices pares automáticos */
+
+function devuelveTrue() {
+    return true
+}
+
+async function damePromesa() {
+    return setTimeout(() => console.log("Hola soy una promesa"), 5000)
+}
+
+function* idsPares() {
+    let id = 0
+    while(true) {
+        yield id += 2
+    }
+}
+
+console.log(devuelveTrue())
+damePromesa()
