dfb5496f3faf4ec15c0241766d7bf6ae972096da
ematevez <70983593+ematevez@users.noreply.github.comb.comgit config --global user.email 70983593+ematevez@users.noreply.github.comb.com>
2022-07-21 19:15:58 -0300

    EJ11 JS

---
+++ Curso-Javascript/11-Programacion-orientada-objetos/poo.js
@@ -0,0 +1,26 @@
+/*
+Crea un nuevo fichero JS que contenga las siguientes líneas
+- Una clase llamada "Estudiante" que tenga:
+- Una variable llamada nombre
+- Otra variable lista llamada asignaturas con 3 asignaturas: Javascript, HTML, CSS
+- Un método "obtenDatos" que devuelva un objeto con las propiedades nombre y asignaturas
+- Crea una nueva instancia de "Estudiante"
+- Haz la llamada al método obtenDatos
+*/
+
+class Estudiante {
+    nombre = "Emanuel"
+    asignaturas = ["Javascript", "HTML", "CSS"]
+
+    obtenDatos() {
+        return {
+            nombre: this.nombre,
+            asignaturas: this.asignaturas
+        }
+    }
+}
+
+const estudiante = new Estudiante()
+
+console.log(estudiante.obtenDatos())
+
