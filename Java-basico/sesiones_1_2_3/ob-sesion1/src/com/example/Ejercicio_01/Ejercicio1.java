9d4a896214312494c729f2082739a3871ba4147e
ematevez <ematevez@gmail.com>
2022-07-26 09:43:28 -0300

    EJ01 JAVA-Basico

---
+++ Java-basico/sesiones_1_2_3/ob-sesion1/src/com/example/Ejercicio_01/Ejercicio1.java
@@ -0,0 +1,35 @@
+package com.example.Ejercicio_01;
+
+public class Ejercicio1 {
+
+    public static void main(String[] args) {
+
+        // enteros
+        byte number1 = 1; // 1 byte
+        short number2 = 10; // 2 bytes
+        int number3 = 50; // 4 bytes
+        long number4 = 100; // 8 bytes
+
+        System.out.println("Numeros enteros: " + number1 + " , " + number2 + " , " + number3 + " , " + number4);
+
+        // decimales
+        float decimal1 = 4.9f;
+        double decimal2 = 9.99d;
+
+        System.out.println("Decimales: " + decimal1 + " , " + decimal2);
+
+        // cadena de texto
+        String nombre = "Emanuel";
+        String apellido = "Apellidos";
+
+        // caracter
+        char caracter1 = 'a';
+
+        System.out.println("Cadena de texto: " + nombre + " " + apellido + "\nCaracter: " + caracter1);
+
+        //booleanos
+        boolean verdadero = true;
+        boolean falso = false;
+    }
+
+}
