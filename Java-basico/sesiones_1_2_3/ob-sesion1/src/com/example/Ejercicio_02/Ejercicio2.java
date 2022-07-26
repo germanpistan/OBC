f29e36d28227edd0eb9e7c9fe6b6f6ea25bbef58
ematevez <ematevez@gmail.com>
2022-07-26 17:52:39 -0300

    Ej02 JAVA_BASICO

---
+++ Java-basico/sesiones_1_2_3/ob-sesion1/src/com/example/Ejercicio_02/Ejercicio2.java
@@ -0,0 +1,20 @@
+package com.example.Ejercicio_02;
+
+import java.util.Scanner;
+public class Ejercicio2 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Introduce un numero: ");
+
+        int numero = scanner.nextInt();
+
+        double iva = numero * 0.21;
+
+        double suma = numero + iva;
+
+        System.out.println("El resultado con IVA es: " + suma);
+    }
+}
