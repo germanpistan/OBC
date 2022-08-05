8fb619bca8bb1f6dd4ab0d21165b6d3daf3309a2
ematevez <ematevez@gmail.com>
2022-08-05 08:55:55 -0300

    EJ JAVA-AV 09-10-11

---
+++ Java-AV/sesion_11/src/Ejercicio_09_10_11/EmailNotificationListener.java
@@ -0,0 +1,16 @@
+package Ejercicio_09_10_11;
+
+import java.io.File;
+
+public class EmailNotificationListener implements EventListener {
+    private String email;
+
+    public EmailNotificationListener(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public void update(String eventType, File file) {
+        System.out.println("Email to " + email + ": Someone has performed " + eventType + " operation with the following file: " + file.getName());
+    }
+}
---
+++ Java-AV/sesion_11/src/Ejercicio_09_10_11/EventListener.java
@@ -0,0 +1,6 @@
+package Ejercicio_09_10_11;
+import java.io.File;
+
+public interface EventListener {
+    void update(String eventType, File file);
+}
